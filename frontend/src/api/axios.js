5866be527a35455ba91635ac235e5707272b9c8e
Sunidhi23garg <100429220+Sunidhi23garg@users.noreply.github.com>
2025-01-11 23:23:25 +0530

    axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,28 +1,3 @@
-// // src/api/axios.js
-// import axios from 'axios';
-
-// const instance = axios.create({
-//   baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-// });
-
-// instance.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem('token');
-//     if (token) {
-//       config.headers['x-auth-token'] = token;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default instance;
-
 
 import axios from 'axios';
 
